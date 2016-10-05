ee1508fc517f3805a7b2922105cf9bd1b4311dc3
Jennifer Jacobs <jacobsj@media.mit.edu>
2016-10-05 09:24:41 -0700

    fixed bug in server logic

--- server.js
+++ server.js
@@ -15,47 +15,49 @@ const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+	.use((req, res) => res.sendFile(INDEX))
+	.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-const wss = new SocketServer({ server });
+const wss = new SocketServer({
+	server
+});
 
 wss.on('connection', (ws) => {
-  console.log('Client connected',ws.protocol);
+	console.log('Client connected', ws.protocol);
 	var protocol = ws.protocol;
-    var connection = ws;
-    var index = clients.push(connection) - 1;
-    var clientName = ws.protocol;
-if(browser_client !== null){
-			browser_client.send(clientName);
-		}
-    if(clientName == 'ipad'){
-    	ipad_client = ws;
-   	}
-   	else if(clientName == 'desktop'){
-    	desktop_client = ws;
-   	}
-   	else if(clientName == 'browser'){
-   		browser_client = ws;
-   	}
+	var connection = ws;
+	var index = clients.push(connection) - 1;
+	var clientName = ws.protocol;
+	if (clientName == 'ipad') {
+		ipad_client = ws;
+	} else if (clientName == 'desktop') {
+		desktop_client = ws;
+	} else if (clientName == 'browser') {
+		browser_client = ws;
+	}
 
 	ws.on('message', function incoming(message) {
-		if(browser_client !== null){
+		if (browser_client !== null) {
 			browser_client.send(message);
 		}
-	var json_data = JSON.parse(message);
-      console.log('message', clientName, json_data.type);
-      if(json_data.type == "gcode" && desktop_client !== null){
-      	desktop_client.send(JSON.stringify(json_data));
-      }
+		var json_data = JSON.parse(message);
+		if (json_data.name == "desktop_client") {
+			browser_client.send("desktop connected");
+			desktop_client = ws;
 
-       ws.send("message recieved");
+		}
+		console.log('message', clientName, json_data.type);
+		if (json_data.type == "gcode" && desktop_client !== null) {
+			desktop_client.send(JSON.stringify(json_data));
+		}
+
+		ws.send("message recieved");
 
 
-  });
- 
+	});
 
-  ws.on('close', () => console.log('Client disconnected'));
+
+	ws.on('close', () => console.log('Client disconnected'));
 });
 
 
